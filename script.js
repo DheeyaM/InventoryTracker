e07f39a045e3b3e76d3984b28a02708c01f82586
Dheeya Manilal <dheeyam7@gmail.com>
2026-01-02 21:41:18 +0200

    Adding to product table

--- script.js
+++ script.js
@@ -29,5 +29,64 @@ addColourBtn.addEventListener("click", () => {
         return;
     }
     colours.push(selectedColour);
-    addedColours.innerText = "Selected Colours:" + colours.join(", ");
+   showColours();
 });
+
+function showColours(){
+    addedColours.innerHTML = "";
+
+    colours.forEach((colour, index) => {
+        const span = document.createElement("span");
+
+        span.innerText = colour + "     ✖ ";
+
+
+        span.addEventListener("click", () =>{
+            //removes 1 element FROM position index
+            colours.splice(index, 1);
+            showColours();
+        })
+
+
+        span.style.cursor = "pointer";
+        addedColours.appendChild(span);
+    })
+}
+
+
+// ADD PRODUCT
+
+function addProduct(){
+    const prodName = document.getElementById("productName").value.trim();
+    const category = document.getElementById("categoryType").value;
+    
+    if (prodName === ""){
+        alert("Please enter a product name!");
+        return;
+    }
+    //get table body
+    const tableBody = document.getElementById("productTable").querySelector("tbody");
+
+    //create row
+    const row = document.createElement("tr");
+
+    //create cells
+    const prodNameCell = document.createElement("td");
+    const categoryCell = document.createElement("td");
+    const colourCell = document.createElement("td");
+
+    prodNameCell.textContent = prodName;
+    categoryCell.textContent = category;
+    colourCell.textContent = colours.join(", ");
+
+    row.appendChild(prodNameCell);
+    row.appendChild(categoryCell);
+    row.appendChild(colourCell);
+
+    tableBody.appendChild(row);
+
+    document.getElementById("productName").value = "";
+}
+
+
+
