2265e678ead0b1a163f6abcb3752f5cd1d06b7f1
Dheeya Manilal <dheeyam7@gmail.com>
2026-01-02 15:48:51 +0200

    product page table and add colour button

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+
+
+// Help button
 const openBtn = document.getElementById("openModal");
 const closeBtn = document.getElementById("closeModal");
 const modal = document.getElementById("modal");
@@ -8,3 +12,22 @@ openBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
     modal.classList.remove("open");
 })
+
+
+// Adding colours
+let colours = [];
+const selectColour = document.getElementById("yarnNeeded");
+const addColourBtn = document.getElementById("addColour");
+const addedColours = document.getElementById("addedColours");
+
+addColourBtn.addEventListener("click", () => {
+    const selectedColour = selectColour.options[selectColour.selectedIndex].text;
+    if (selectedColour === "Yarn"){
+        return;
+    }
+    if (colours.includes(selectedColour)){
+        return;
+    }
+    colours.push(selectedColour);
+    addedColours.innerText = "Selected Colours:" + colours.join(", ");
+});
